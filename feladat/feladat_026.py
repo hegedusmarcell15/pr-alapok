9b2178c3ac616abeb96e0f12615aa5000bf6d66f
hegedusmarcell15 <128583323+hegedusmarcell15@users.noreply.github.com>
2023-12-14 10:28:54 +0100

    Add files via upload

---
+++ feladat/feladat_026.py
@@ -0,0 +1,13 @@
+
+tantargyak = ['matek', 'tori', 'kémia', 'bio', 'kémia', 'info']
+
+for tantargy in tantargyak:
+    print(tantargy)
+
+
+
+index = 0
+for tantargy in tantargyak:
+    print(f'index: {index}, a tantargy neve:{tantargy}')
+    index = index + 1
+  
---
+++ feladat/feladat_027.py
@@ -0,0 +1,7 @@
+honapok = ['január', 'február','március', 'április', 'május', 'június'] 
+  
+
+for index in range(len(honapok)):
+    print(index, honapok[index])
+
+print(f"A lista 0. elemének típusa: {type(honapok[0])}")
