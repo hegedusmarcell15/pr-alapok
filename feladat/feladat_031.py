491fa76c58d0096da116961141588bea2fef2f66
hegedusmarcell15 <128583323+hegedusmarcell15@users.noreply.github.com>
2024-01-24 08:02:43 +0100

    Add files via upload

---
+++ feladat/feladat_031.py
@@ -0,0 +1,12 @@
+# feladat_031
+
+# Amit már tudtunk a szövegekről (sztringekről):
+
+# Szövegek (sztringek) összefűzhetőek
+print('Jöttem' + ' ' + 'láttam' + ' ' + 'győztem.')
+
+# Szövegek (sztringek) szorzásjellel megsokszorozhatóak
+print('ja' + 'j' * 7)
+
+# Más adattípusok szöveggé (sztringgé) alakíthatóak az str() függvénnyel
+print(str(7.53))
---
+++ feladat/feladat_032.py
@@ -0,0 +1,13 @@
+# feladat_032
+
+Listák rendezése
+
+    nyelvek = ['Python', 'C', 'C++', 'Java']
+
+    # sorbarendezi a listát
+    nyelvek.sort()
+
+    # fordított sorrendbe rendezi a listát
+    nyelvek.reverse()  
+ 
+A listák a sort() és reverse() metódusok hatására módosulnak, elemeik rendezett sorban helyezkednek el.
